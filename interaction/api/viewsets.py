c4a1b2c1d72cb6fbbe3698d16e5ad88c13841113
João Dantas <joaodantastx1@gmail.com>
2024-11-05 09:53:57 -0300

    reformulando viewset de interação

--- interaction/api/viewsets.py
+++ interaction/api/viewsets.py
@@ -7,3 +7,13 @@ class InteractionViewSet(viewsets.ModelViewSet):
     queryset = Interaction.objects.all()
     serializer_class = InteractionSerializer
     permission_classes = [permissions.IsAuthenticated, IsInSpecificGroup, permissions.DjangoModelPermissions]
+    def get_queryset(self):
+        # Obter o parâmetro 'pixelPost' da URL, se estiver presente
+        pixel_id = self.request.query_params.get('pixelPost', None)
+        
+        # Se 'pixelPost' estiver presente, filtrar as interações associadas a esse Pixel específico
+        if pixel_id is not None:
+            return Interaction.objects.filter(pixelPost_id=pixel_id)
+        
+        # Caso contrário, retornar todas as interações
+        return Interaction.objects.all()
